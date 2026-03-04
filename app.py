ea0a5c7c562e94f8a0630edafc079ea7dc83d4aa
vrushali ithape <vrushaliithape123@gmail.com>
2026-03-04 19:38:43 +0530

    Refactor image upload and model prediction logic

--- app.py
+++ app.py
@@ -6,17 +6,15 @@ st.title("YOLOv8 Cloud Object Detection")
 
 @st.cache_resource
 def load_model():
-    return YOLO("best.pt")   # use your trained model
+    return YOLO("best.pt")
 
 model = load_model()
 
 uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "png", "jpeg"])
 
-if uploaded_file is not None:
+if uploaded_file:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image")
 
-    results = model.predict(source=image)
-    res_plotted = results[0].plot()
-
-    st.image(res_plotted, caption="Detected Image")
+    results = model(image)
+    st.image(results[0].plot(), caption="Detected Image")
